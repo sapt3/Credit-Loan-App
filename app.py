86c582783505302cc725b220589391c0ddce5c21
rahul <saptarshi.ironmaiden@gmail.com>
2019-08-04 14:18:06 +0530

    Added support for income credit

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 # Creates a program which calculates the down payment of a real estate project
 house_price = 1000000
 has_good_credit = True
+has_high_income = True
+if has_good_credit or has_high_income:
+    print("Eligible for loan")
+    if has_good_credit:
+        down_payment = house_price * 0.1
 
-if has_good_credit:
-    down_payment = house_price * 0.1
-
+    else:
+        down_payment = house_price * 0.2
 else:
-    down_payment = house_price * 0.2
+    print("Not eligible for loan")
+
 
 print("The downpayment required to be paid is $" + str(down_payment))
